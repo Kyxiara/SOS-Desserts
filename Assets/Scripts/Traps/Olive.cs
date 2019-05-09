7aaae198901614d7ee46f5be05b9e6c31c050ff1
Kelly N <kellynarboux@hotmail.fr>
2019-05-09 14:03:35 +0200

    WIP: explosion olive

--- Assets/Scripts/Traps/Olive.cs
+++ Assets/Scripts/Traps/Olive.cs
@@ -4,13 +4,59 @@ using UnityEngine;
 
 public class Olive : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Rigidbody2D myRigidbody;
+    private bool explode;
+    private float explodeCounter;
+
+    [SerializeField]
+    private float explodeLength;
+
+    [SerializeField]
+    private float sizeGrow;
+
+    [SerializeField]
+    private GameObject particlesExplosion;
+
+    private GameObject player;
+
+    [SerializeField]
+    private float distanceStart;
+
+    // Use this for initialization
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        explode = false;
+        player = GameObject.Find("Player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (explode) Explode();
+        if (Vector2.Distance(player.transform.position, transform.position) < distanceStart && !explode)
+        {
+            explode = true;
+            explodeCounter = explodeLength;
+        }
+    }
+
+    private void Explode()
+    {
+        if (explodeCounter > explodeLength * .80f)
+            myRigidbody.transform.localScale += new Vector3(sizeGrow, sizeGrow, 0);
+        else if (explodeCounter > explodeLength * .65f)
+            myRigidbody.transform.localScale -= new Vector3(sizeGrow, sizeGrow, 0);
+        else if (explodeCounter > explodeLength * .45)
+            myRigidbody.transform.localScale += new Vector3(sizeGrow, sizeGrow, 0);
+        else if (explodeCounter > explodeLength * .30f)
+            myRigidbody.transform.localScale -= new Vector3(sizeGrow, sizeGrow, 0);
+        else if (explodeCounter > 0f)
+            myRigidbody.transform.localScale += new Vector3(sizeGrow, sizeGrow, 0);
+        else
+        {
+            GameObject particles = Instantiate(particlesExplosion, transform.position, Quaternion.identity);
+            particles.GetComponent<ParticleSystem>().Play();
+            Destroy(gameObject);
+        }
+        explodeCounter -= Time.deltaTime;
+    }
 }
