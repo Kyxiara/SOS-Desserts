e266e3d6eaf02fcd4d454ff5823ce056b2dbfa05
Kelly N <kellynarboux@hotmail.fr>
2019-05-10 13:05:19 +0200

    Explosion olive finished

--- Assets/Scripts/Traps/ExplosionOlive.cs
+++ Assets/Scripts/Traps/ExplosionOlive.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ExplosionOlive : MonoBehaviour {
 
     private ParticleSystem ps;
+    private bool playerTouched;
 
     // Use this for initialization
     void Start () {
         ps = GetComponent<ParticleSystem>();
+        playerTouched = false;
     }
 	
 	// Update is called once per frame
@@ -19,6 +21,15 @@ public class ExplosionOlive : MonoBehaviour {
             {
                 Destroy(gameObject);
             }
+        }
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (other == GameObject.Find("Player") && !playerTouched)
+        {
+            playerTouched = true;
+            StartCoroutine(other.GetComponent<Player>().TakeDamage());
         }
     }
 }
